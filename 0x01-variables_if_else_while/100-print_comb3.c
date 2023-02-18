2caab6bd6aba9a7a2e719a206306afb36ed58499
brucetravis <bruceambundo@gmail.com>
2023-02-17 20:51:37 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - prints the alphabetic
+ *Return: Always (Success)
+ */
+int main(void)
+
+{/**
+ *main - prints the alphabetic
+ *Return: Always (Success)
+ */
+int main(void)
+
+{
+int d, p;
+for (d = '0'; d < '9'; d++)
+{
+for (p = d + 1; p <= '9'; p++)
+{
