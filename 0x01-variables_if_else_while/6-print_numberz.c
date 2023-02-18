517edb109d0eae7c18ae792c84c48fb39b73169d
brucetravis <bruceambundo@gmail.com>
2023-02-17 19:12:01 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - prints the alphabetic
+ *Return: Always (Success)
+ */
+int main(void)
+
+{
+int d;
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}
