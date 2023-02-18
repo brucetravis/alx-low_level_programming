11cf7c705776fe179a2bb59dba3b170146c2ba08
brucetravis <bruceambundo@gmail.com>
2023-02-17 20:01:47 -0800

    8-print_base16

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - prints the alphabetic
+ *Return: Always (Success)
+ */
+int main(void)
+
+{
+int d;
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
