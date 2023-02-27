dae10ef4e30306db2abd887cbe90bd2b631aa52d
brucetravis <bruceambundo@gmail.com>
2023-02-27 08:42:35 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,9 +14,23 @@ int min = 1;
 int isi = 0;
 
 while (s[c])
-
-if (s[c]) == 45)
+{
+if (s[c] == 45)
 {
 min *= -1;
 }
 while (s[c] >= 48 && s[c] <= 57)
+{
+isi = 1;
+ni = (ni * 10) + (s[c] - '0');
+c++;
+}
+if (isi == 1)
+{
+break;
+}
+c++;
+}
+ni *= min;
+return (ni);
+}
