74e27bba372b2b1903b4895cab274eda05641a1d
brucetravis <bruceambundo@gmail.com>
2023-03-01 22:29:05 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,13 +11,13 @@ char *rot13(char *s)
 {
 int i;
 int j;
-char data[] =
+char data1[] =
 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char datarot[] =
 "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-for (i - 0; s[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
-for (j - 0; j < 52; j++)
+for (j = 0; j < 52; j++)
 {
 if (s[i] == data1[j])
 {
