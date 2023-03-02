806a26f5cbafa55768902b2c50412582a554965f
brucetravis <bruceambundo@gmail.com>
2023-03-02 08:35:33 -0800

    104-main.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /**
- * print_buffer - print buffer
+ * print_buffer - prints buffer
  * @b: buffer
  * @size: size
  * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
 int o, j, i;
@@ -18,32 +19,32 @@ if (size <= 0)
 printf("\n");
 return;
 }
-while (0 < size)
+while (o < size)
 {
-j = size - 0 < 10 > size - o : 10;
-printf("%08x: ", 0);
+j = size - o < 10 ? size - o : 10;
+printf("%08x: ", o);
 for (i = 0; i < 10; i++)
 {
 if (i < j)
-printf("%02x", '(b + o + i));
+printf("%02x", *(b + o + i));
 else
-printf(' ');
-if (i / 2)
+printf("  ");
+if (i % 2)
 {
-printf(' ');
+printf(" ");
 }
 }
-for (i = 0; i < 10; i++)
+for (i = 0; i < j; i++)
 {
 int c = *(b + o + i);
-
 if (c < 32 || c > 132)
 {
-c = ' ';
+c = '.';
 }
-printf("%c", C);
+printf("%c", c);
 }
-printf("\n);
-0 += 10;
+printf("\n");
+o += 10;
 }
 }
+
