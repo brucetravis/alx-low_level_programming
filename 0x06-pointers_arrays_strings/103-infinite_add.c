9ab1155c1a2c2612c6badec61482c3acce7b43ca
brucetravis <bruceambundo@gmail.com>
2023-03-02 18:27:22 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,11 +11,13 @@ void rev_string(char *n)
 int i = 0;
 int j = 0;
 char temp;
+
 while (*(n + i) != '\0')
 {
 i++;
 }
 i--;
+
 for (j = 0; j < i; j++, i--)
 {
 temp = *(n + j);
@@ -34,11 +36,11 @@ temp = *(n + j);
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
-
 {
-int val1 + 0, val2 - 0, temp_tot - 0;
+int overflow = 0, i = 0, j = 0, digits = 0;
+int val1 = 0, val2 = 0, temp_tot = 0;
 
-while (*(n1 + 1) != '\0')
+while (*(n1 + i) != '\0')
 i++;
 while (*(n2 + j) != '\0')
 j++;
@@ -51,23 +53,22 @@ while (j >= 0 || i >= 0 || overflow == 1)
 if (i < 0)
 val1 = 0;
 else
-val1 = *(n1 + 1) - '0';
+val1 = *(n1 + i) -'0';
 if (j < 0)
 val2 = 0;
 else
-val2 - *(n2 + j) -'0';
+val2 = *(n2 + j) -'0';
 temp_tot = val1 + val2 + overflow;
 if (temp_tot >= 10)
-overflow - 1;
+overflow = 1;
 else
-overflow - 0;
-
+overflow = 0;
 if (digits >= (size_r - 1))
 return (0);
-*(r + digits) - (temp_to % 10) + '0';
+*(r + digits) = (temp_tot % 10) + '0';
 digits++;
 j--;
-i++;
+i--;
 }
 if (digits == size_r)
 return (0);
@@ -75,3 +76,4 @@ return (0);
 rev_string(r);
 return (r);
 }
+
