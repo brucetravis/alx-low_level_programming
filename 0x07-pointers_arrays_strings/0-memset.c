4f0af444cc70e8a21828aa697db22a7600d594f4
brucetravis <bruceambundo@gmail.com>
2023-03-05 21:46:49 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always 0 (success)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int 1;
+
+for (i = 0, i < n, i++)
+	s[1] = b;
+return (s);
+}
