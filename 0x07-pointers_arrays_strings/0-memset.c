2f2c435b041afcb7f015db053e389b5ade6cb316
brucetravis <bruceambundo@gmail.com>
2023-03-05 22:14:00 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,9 +10,9 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int 1;
+unsigned int i;
 
-for (i = 0, i < n, i++)
-	s[1] = b;
+for (i = 0; i < n; i++)
+	s[i] = b;
 return (s);
 }
