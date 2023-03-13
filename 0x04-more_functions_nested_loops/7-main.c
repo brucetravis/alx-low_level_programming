e865e348296e857b7abec59c3d247a91cc3b94e8
brucetravis <bruceambundo@gmail.com>
2023-03-13 00:55:40 -0700

    0-create_array.c

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -8,9 +8,9 @@
 
 int main(void)
 {
-void print_diagonal(0);
-     print_diagonal(2);
-     print_diagonal(10);
-     print_diagonal(-4);
-return (0);
+     void print_diagonal(0);
+          print_diagonal(2);
+          print_diagonal(10);
+          print_diagonal(-4);
+          return (0);
 }
