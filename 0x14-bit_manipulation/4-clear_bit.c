ebdddca6f9aa2a66efca3c3db7b7b1de5269231c
brucetravis <bruceambundo@gmail.com>
2023-04-08 00:00:35 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,9 +9,13 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-if (index > 63)
+unsigned long int mask = 1UL << index;
+if (index >= sizeof(unsigned long int) * 8)
+{
 return (-1);
+}
 
-*n = (~(1UL << index) & *n);
+mask = 1UL << index;
+*n &= ~mask;
 return (1);
 }
