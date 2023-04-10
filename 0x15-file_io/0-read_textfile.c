955639ed5676099e7c7e629935aa04ec1563ec82
brucetravis <bruceambundo@gmail.com>
2023-04-10 08:59:26 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,57 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output
- *
- * @filename: The name of the file to read
- * @letters: The number of letters to read and print
- *
- * Return: The actual number of letters read and
- * printed, or 0 if an error occurred
+ * read_textfile- Read text file print to STDOUT using putchar.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd, bytes_read;
-char *buffer;
+char *buf;
+ssize_t fd;
+ssize_t t;
+ssize_t w = 0;
+ssize_t i;
 
-if (filename == NULL)
+fd = open(filename, O_RDONLY);
+if (fd == -1)
 return (0);
 
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
 
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
-return (0);
-
-bytes_read = read(fd, buffer, letters);
-if (bytes_read == -1)
+buf = malloc(sizeof(char) * letters);
+if (buf == NULL)
 {
-free(buffer);
-return (0);
-}
-
-ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-free(buffer);
 close(fd);
-
-if (bytes_written != bytes_read)
 return (0);
-
-return (bytes_written);
 }
 
+t = read(fd, buf, letters);
+if (t == -1)
+{
+free(buf);
+close(fd);
+return (0);
+}
+
+for (i = 0; i < t; i++)
+{
+if (putchar(buf[i]) == EOF)
+{
+free(buf);
+close(fd);
+return (0);
+}
+w++;
+}
+
+free(buf);
+close(fd);
+return (w);
+}
