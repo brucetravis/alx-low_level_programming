9e93cfa51b46a20fde69786410b3c762ba1210b2
brucetravis <bruceambundo@gmail.com>
2023-06-07 11:21:57 -0700

    4-pow_recursion.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -15,18 +15,18 @@ int i;
 
 if (n < 0)
 {
-return -1;
+return (-1);
 }
 
 if (n == 0)
 {
-return 1;
+return (1);
 }
 
 for (i = 1; i <= n; i++)
 {
-result *= i;
+result(*= i);
 }
 
-return result;
-}	
+return (result);
+}
