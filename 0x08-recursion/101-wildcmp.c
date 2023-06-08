d389fc2aa40882b2ee98f67b816c1c67c4899cb5
brucetravis <bruceambundo@gmail.com>
2023-06-08 11:17:28 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,19 +12,27 @@
 
 int wildcmp(char *s1, char *s2)
 {
-if (*s1 == '\0')
+
+if (*s2 == '\0')
 {
-if (*s2 != '\0' && *s2 == '*')
+return (*s1 == '\0');
+}
+else if (*s2 == '*')
 {
-return (wildcmp(s1, s2 + 1));
+if (*(s2 + 1) == '\0')
+{
+return (1);
 }
-return (*s2 == '\0');
-}
-if (*s2 == '*')
+else if (*s1 != '\0')
 {
 return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 }
-if (*s1 == *s2)
+else
+{
+return (wildcmp(s1, s2 + 1));
+}
+}
+else if (*s1 == *s2)
 {
 return (wildcmp(s1 + 1, s2 + 1));
 }
