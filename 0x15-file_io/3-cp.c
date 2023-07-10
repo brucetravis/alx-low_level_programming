b4a2b8f6f0b3a9d76333cbae22ca434c0e270220
brucetravis <bruceambundo@gmail.com>
2023-07-10 06:11:40 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,7 @@
 #include "main.h"
+
 #define BUFFER_SIZE 1024
+
 /**
  * print_error - prints an error message to the POSIX standard error.
  * @message: error message to print.
@@ -8,63 +10,92 @@ void print_error(const char *message)
 {
 dprintf(STDERR_FILENO, "%s\n", message);
 }
+
 /**
- * main - entry point of the program.
- * @argc: number of command-line arguments.
- * @argv: array of command-line arguments.
+ * open_file - opens a file with the specified flags and mode.
+ * @filename: name of the file to open.
+ * @flags: flags to use when opening the file.
+ * @mode: mode to use when creating the file (if applicable).
  *
- * Return: 0 on success, or the corresponding error code on failure.
+ * Return: the file descriptor on success,
+ * or exits with an error code on failure.
  */
-int main(int argc, char **argv)
+int open_file(const char *filename, int flags, mode_t mode)
 {
-int fd_from, fd_to, bytes_read, bytes_written;
+int fd = open(filename, flags, mode);
+if (fd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't open file %s\n", filename);
+exit(98);
+}
+return (fd);
+}
+
+/**
+ * copy_file - copies the contents of one file to another.
+ * @fd_from: file descriptor of the source file.
+ * @fd_to: file descriptor of the destination file.
+ *
+ * Return: void. Exits with an error code on failure.
+ */
+void copy_file(int fd_from, int fd_to)
+{
 char buffer[BUFFER_SIZE];
-if (argc != 3)
-{
-print_error("Usage: cp file_from file_to");
-return (97);
-}
-fd_from = open(argv[1], O_RDONLY);
-if (fd_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-return (98);
-}
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-if (fd_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(fd_from);
-return (99);
-}
+ssize_t bytes_read, bytes_written;
+
 while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 {
 bytes_written = write(fd_to, buffer, bytes_read);
 if (bytes_written == -1 || bytes_written != bytes_read)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(fd_from);
-close(fd_to);
-return (99);
+dprintf(STDERR_FILENO, "Error: Can't write to file\n");
+exit(99);
 }
 }
+
 if (bytes_read == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-close(fd_from);
-close(fd_to);
-return (98);
+dprintf(STDERR_FILENO, "Error: Can't read from file\n");
+exit(98);
 }
+}
+
+/**
+ * main - entry point of the program.
+ * @argc: number of command-line arguments.
+ * @argv: array of command-line arguments.
+ *
+ * Description: This program copies the contents of one file to another.
+ *
+ * Return: 0 on success, or the corresponding error code on failure.
+ */
+int main(int argc, char **argv)
+{
+int fd_from, fd_to;
+
+if (argc != 3)
+{
+print_error("Usage: cp file_from file_to");
+exit(97);
+}
+
+fd_from = open_file(argv[1], O_RDONLY, 0);
+fd_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+
+copy_file(fd_from, fd_to);
+
 if (close(fd_from) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-close(fd_to);
-return (100);
+dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
+exit(100);
 }
+
 if (close(fd_to) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-return (100);
+dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
+exit(100);
 }
+
 return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
+void print_error(const char *message);
+int main(int argc, char **argv);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
